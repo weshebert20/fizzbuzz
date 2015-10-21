3a6753958896538050c9df9b25b99df4d7c19d4a
Wesley <weshebert20@gmail.com>
2015-10-20 21:17:16 -0600

    index.html

--- js/app.js
+++ js/app.js
@@ -1,20 +1,40 @@
-$(document).ready(function() {
-	$("body").on("click", "button", function (event) {
+$(document).ready(function(){
 
-		for ( var i = 1; i < 101; i++) {
-		
-			if (i % 3 === 0 && i % 5 === 0) {
-				$(".body1").append("<p>FizzBuzz</p>");
+	function fizzbuzz(max) {
+		var count = 1;
+		for (var i = 1; i <= max; i++) {
+			if (i % 5 == 0 && i % 3 == 0) {
+      			$(".body1").append("<p>" + count + ": " + "FizzBuzz" + "</p>");
+      			count += 1;
+    		} 
+    		else if ( i % 3 == 0 ) {
+      			$(".body1").append("<p>" + count + ": " + "Fizz" + "</p>");
+      			count += 1;
+    		} 
+    		else if (i % 5 == 0) {
+      			$(".body1").append("<p>" + count + ": " + "Buzz" + "</p>");
+      			count += 1;
+    		} 
+    		else {
+      			$(".body1").append("<p>" + count + ": " + i + "</p>");
+      			count += 1;
+    		}
+		}
+	}
+
+	function runFizzbuzz() {
+		$('input').keydown(function(enter){
+			if (enter.keyCode == 13) {
+				enter.preventDefault();
+				$('p').remove();
+				var userInput = $(this).val();
+				var stringNum = parseInt(userInput, 10);
+				var num = + stringNum;
+				fizzbuzz(num);
 			}
-			else if ( i % 3 === 0) {
-				$(".body1").append("<p>Fizz</p>");
-			}
-			else if ( i % 5 === 0) {
-				$(".body1").append("<p>Buzz</p>");
-			}
-			else {
-				$(".body1").append("<p>" + i + "</p>")
-			}
-		};	
-	});
+		})
+	}
+
+	runFizzbuzz();
+
 });
